444d0c8c9ad03d9843f0e5a964081d27191530d1
Jose Luis Díaz Alonso <134778793+JoseLuisDiazDev@users.noreply.github.com>
2023-12-28 20:50:59 +0100

    Este es el JavaScript de App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+import Inicio from '../src/pages/Inicio';
+import Servicios from '../src/pages/Servicios';
+import Contacto from '../src/pages/Contacto';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/Servicios" element={<Servicios />} />
+          <Route path="/Contacto" element={<Contacto />} />
+        </Routes>
+      </Router>
+
+
+      <Footer />
     </div>
+
   );
 }
 
