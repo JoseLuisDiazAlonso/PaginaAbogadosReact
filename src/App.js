a60dc1d712752303990d2a2b11669cff8e608771
Jose Luis Díaz Alonso <134778793+JoseLuisDiazDev@users.noreply.github.com>
2023-12-30 21:02:20 +0100

    Hemos solucionado algunos problemas con el Enroutador

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// App.js
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../src/components/Header';
@@ -6,24 +8,19 @@ import Inicio from '../src/pages/Inicio';
 import Servicios from '../src/pages/Servicios';
 import Contacto from '../src/pages/Contacto';
 
-
-
 function App() {
   return (
-    <div>
-      <Header />
-      <Router>
+    <Router>
+      <div>
+        <Header />
         <Routes>
           <Route path="/" element={<Inicio />} />
           <Route path="/Servicios" element={<Servicios />} />
           <Route path="/Contacto" element={<Contacto />} />
         </Routes>
-      </Router>
-
-
-      <Footer />
-    </div>
-
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
